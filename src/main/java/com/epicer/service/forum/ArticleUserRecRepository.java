6811f7ec45bcfc7514ba585cab078e8d9305d062
Lai <jasont11413@gmail.com>
2022-09-16 10:43:48 +0800

    9/16 10:43上傳

--- src/main/java/com/epicer/service/forum/ArticleUserRecRepository.java
+++ src/main/java/com/epicer/service/forum/ArticleUserRecRepository.java
@@ -1,9 +1,17 @@
 package com.epicer.service.forum;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.epicer.model.forum.ArticleBean;
 import com.epicer.model.forum.ArticleCollectRecBean;
+import com.epicer.model.forum.ArticleReplyBean;
 
 public interface ArticleUserRecRepository extends JpaRepository<ArticleCollectRecBean, Integer> {
+	
+	public List<ArticleBean> findArticleByUserIdLike(int userid);
+
+	public List<ArticleReplyBean> findReplyByUserIdLike(int userid);
 
 }
--- src/main/java/com/epicer/service/forum/ArticleUserRecService.java
+++ src/main/java/com/epicer/service/forum/ArticleUserRecService.java
@@ -7,24 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.epicer.model.forum.ArticleBean;
+import com.epicer.model.forum.ArticleReplyBean;
+
 
 @Service
 @Transactional
 public class ArticleUserRecService{
 
 	@Autowired
-	private ArticleUserRecDao aurDao;
+	private ArticleUserRecRepository aurRepo;
 
 
 
 	public List<ArticleBean> selectArticle(int userid) {
 		// TODO Auto-generated method stub
-		return  aurDao.selectArticleRec(userid);
+		return  aurRepo.findArticleByUserIdLike(userid);
 	}
 
 	public List<ArticleReplyBean> selectReply(int userid) {
 		// TODO Auto-generated method stub
-		return  aurDao.selectReplyRec(userid);
+		return  aurRepo.findReplyByUserIdLike(userid);
 	}
 
 
